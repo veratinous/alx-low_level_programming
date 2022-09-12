fc0d4bc54e97b1cf2aaa8f4fadcf05baa38194aa
veratinous <veratinous@gmail.com>
2022-09-12 08:59:52 -0700

    quick fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
 /**
- *
  *  main - Entry point
  *
  *  Description: print 0-9 using putchar
  *
  *     while using int variable
- * 
+ *
  *       * Return: Always 0 (Success)
 */
 
