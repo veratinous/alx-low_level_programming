11c939e503404524b30c3886932f8b60013cb5bb
veratinous <veratinous@gmail.com>
2022-09-13 05:29:40 -0700

    task 1 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing putchar to stdout
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
