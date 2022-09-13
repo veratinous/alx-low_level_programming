7d08b4ec72eff14839275369d0fba921fa9d0679
veratinous <veratinous@gmail.com>
2022-09-13 16:14:30 -0700

    printing lowercase letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase letters
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	int count;
+
+	while (ch < 'z'; ++ch)
+		_putchar(ch);
+
+	_putchar('\n');
+	return (0);
+}
