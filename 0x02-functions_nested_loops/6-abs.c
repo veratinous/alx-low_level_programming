9e572dd2338bf960910fb94042f8ac29459dabe0
veratinous <veratinous@gmail.com>
2022-09-14 03:41:46 -0700

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * @n: number
+ *
+ * Return: 0 (success)
+*/
+
+int _abs(int n)
+{
+	_putchar((unsigned int)n);
+
+	return (0);
+}
